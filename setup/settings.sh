6e7450f256df44f4903edebd9e42833619e5563c
kplachkov <k.n.plachkov@gmail.com>
2019-12-14 20:10:26 +0200

    Add custom keybindings

--- setup/settings.sh
+++ setup/settings.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Input sources.
-gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('xkb', 'bg+phonetic')]"
-gsettings set org.gnome.desktop.input-sources per-window true
 # Keybindings.
+gsettings set org.gnome.settings-daemon.plugins.media-keys terminal "[]"
 gsettings set org.gnome.desktop.wm.keybindings show-desktop "['<Super>d']"
 gsettings set org.gnome.desktop.wm.keybindings switch-applications "[]"
 gsettings set org.gnome.desktop.wm.keybindings switch-group "[]"
@@ -12,19 +10,44 @@ gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Supe
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['<Shift><Super>Above_Tab', '<Control><Alt>Up']"
 gsettings set org.gnome.desktop.wm.keybindings switch-windows "[]"
 gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "[]"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "[]"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "[]"
 gsettings set org.gnome.desktop.wm.keybindings cycle-windows "['<Super>Tab']"
 gsettings set org.gnome.desktop.wm.keybindings cycle-windows-backward "['<Shift><Super>Tab']"
 gsettings set org.gnome.desktop.wm.keybindings toggle-fullscreen "['<Primary>F11']"
 gsettings set org.gnome.desktop.wm.keybindings close "['<Primary>space']"
+
+# Custom keybindings.
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/']"
+# Scripts directory path.
+setup_dir=$(dirname "$(readlink -f "$0")")
+scripts_dir=${setup_dir/setup/scripts}
+# Shutdown computer.
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "shutdown"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "shutdown now"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "<Primary><Alt>End"
+# Start tmux.
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name "tmux"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command "gnome-terminal --full-screen -- $scripts_dir/run_tmux.sh"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding "<Primary><Alt>t"
+
+# Input sources.
+gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('xkb', 'bg+phonetic')]"
+gsettings set org.gnome.desktop.input-sources per-window true
+
 # Night light.
 gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
 gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-automatic false
 gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-from 7
 gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-to 5
+
 # Power.
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing'
 gsettings set org.gnome.desktop.session idle-delay 120
+
 # Appearance.
 gsettings set org.gnome.desktop.interface icon-theme "Numix-Circle"
 gsettings set org.gnome.desktop.interface cursor-theme "Yaru"
@@ -33,10 +56,13 @@ gsettings set org.gnome.desktop.interface cursor-size 64
 gsettings set org.gnome.desktop.interface clock-show-date true
 gsettings set org.gnome.desktop.interface clock-show-weekday true
 gsettings set org.gnome.desktop.interface show-battery-percentage true
+
 # Privacy.
 gsettings set org.gnome.desktop.privacy send-software-usage-stats false
 gsettings set org.gnome.desktop.privacy report-technical-problems false
+
 # Show hidden files.
 gsettings set org.gtk.Settings.FileChooser show-hidden true
+
 # Favorite applications.
 gsettings set org.gnome.shell favorite-apps "['google-chrome.desktop', 'org.gnome.Terminal.desktop', 'syntevo-smartgit.desktop', 'sublime_text.desktop']"
