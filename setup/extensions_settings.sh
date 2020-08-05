304df7b9713f3a1058595736812e17bbb866805d
kplachkov <k.n.plachkov@gmail.com>
2020-08-05 21:25:38 +0200

    Update gnome extension settings

--- setup/extensions_settings.sh
+++ setup/extensions_settings.sh
@@ -1,9 +1,9 @@
 # Dash to dock.
-gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode 'FIXED'                                                                       
-gsettings set org.gnome.shell.extensions.dash-to-dock shortcut-text '<Super>q'                                                                        
-gsettings set org.gnome.shell.extensions.dash-to-dock apply-custom-theme true                                                                         
-gsettings set org.gnome.shell.extensions.dash-to-dock show-favorites true
-gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'focus-or-previews'
+gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode 'DYNAMIC'
+gsettings set org.gnome.shell.extensions.dash-to-dock shortcut-text '<Super>q'
+gsettings set org.gnome.shell.extensions.dash-to-dock apply-custom-theme false
+gsettings set org.gnome.shell.extensions.dash-to-dock apply-glossy-effect true
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'cycle-windows'
 gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-9 "['<Super>9']"
 gsettings set org.gnome.shell.extensions.dash-to-dock custom-background-color false
 gsettings set org.gnome.shell.extensions.dash-to-dock app-shift-hotkey-1 "['<Shift><Super>1']"
@@ -19,7 +19,7 @@ gsettings set org.gnome.shell.extensions.dash-to-dock app-shift-hotkey-2 "['<Shi
 gsettings set org.gnome.shell.extensions.dash-to-dock show-windows-preview true
 gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
 gsettings set org.gnome.shell.extensions.dash-to-dock app-ctrl-hotkey-2 "['<Ctrl><Super>2']"
-gsettings set org.gnome.shell.extensions.dash-to-dock autohide-in-fullscreen false 
+gsettings set org.gnome.shell.extensions.dash-to-dock autohide-in-fullscreen true
 gsettings set org.gnome.shell.extensions.dash-to-dock icon-size-fixed true
 gsettings set org.gnome.shell.extensions.dash-to-dock scroll-action 'switch-workspace'
 gsettings set org.gnome.shell.extensions.dash-to-dock shift-middle-click-action 'launch'
@@ -29,44 +29,45 @@ gsettings set org.gnome.shell.extensions.dash-to-dock pressure-threshold 200.0
 gsettings set org.gnome.shell.extensions.dash-to-dock hide-delay 0.10000000000000002
 gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top false
 gsettings set org.gnome.shell.extensions.dash-to-dock app-ctrl-hotkey-3 "['<Ctrl><Super>3']"
-gsettings set org.gnome.shell.extensions.dash-to-dock show-show-apps-button true
+gsettings set org.gnome.shell.extensions.dash-to-dock show-favorites true
 gsettings set org.gnome.shell.extensions.dash-to-dock app-shift-hotkey-4 "['<Shift><Super>4']"
 gsettings set org.gnome.shell.extensions.dash-to-dock show-running true
 gsettings set org.gnome.shell.extensions.dash-to-dock force-straight-corner false
 gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-1 "['<Super>1']"
 gsettings set org.gnome.shell.extensions.dash-to-dock app-ctrl-hotkey-4 "['<Ctrl><Super>4']"
-gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-style 'DOTS'
+gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-style 'DASHES'
 gsettings set org.gnome.shell.extensions.dash-to-dock hotkeys-overlay true
 gsettings set org.gnome.shell.extensions.dash-to-dock app-shift-hotkey-5 "['<Shift><Super>5']"
-gsettings set org.gnome.shell.extensions.dash-to-dock animation-time 0.10000000000000002                                                              
-gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-running-dots-color '#e95420'                                                       
-gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-dominant-color false                                                          
-gsettings set org.gnome.shell.extensions.dash-to-dock hotkeys-show-dock false                                                                         
+gsettings set org.gnome.shell.extensions.dash-to-dock animation-time 0.10000000000000002
+gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-running-dots-color '#e95420'
+gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-dominant-color false
+gsettings set org.gnome.shell.extensions.dash-to-dock hotkeys-show-dock false
 gsettings set org.gnome.shell.extensions.dash-to-dock app-ctrl-hotkey-5 "['<Ctrl><Super>5']"
-gsettings set org.gnome.shell.extensions.dash-to-dock unity-backlit-items false                                                                       
-gsettings set org.gnome.shell.extensions.dash-to-dock animate-show-apps true                                                                          
+gsettings set org.gnome.shell.extensions.dash-to-dock show-show-apps-button true
+gsettings set org.gnome.shell.extensions.dash-to-dock animate-show-apps true
+gsettings set org.gnome.shell.extensions.dash-to-dock unity-backlit-items false
 gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-2 "['<Super>2']"
-gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-running-dots-border-width 0                                                        
+gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-running-dots-border-width 0
 gsettings set org.gnome.shell.extensions.dash-to-dock app-shift-hotkey-6 "['<Shift><Super>6']"
 gsettings set org.gnome.shell.extensions.dash-to-dock shortcut "['<Super>q']"
-gsettings set org.gnome.shell.extensions.dash-to-dock customize-alphas false                                                                          
+gsettings set org.gnome.shell.extensions.dash-to-dock customize-alphas true
 gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-3 "['<Super>3']"
 gsettings set org.gnome.shell.extensions.dash-to-dock app-ctrl-hotkey-6 "['<Ctrl><Super>6']"
-gsettings set org.gnome.shell.extensions.dash-to-dock autohide true                                                                                   
-gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts true                                                                                
-gsettings set org.gnome.shell.extensions.dash-to-dock minimize-shift true                                                                             
-gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'                                                                          
+gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
+gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts true
+gsettings set org.gnome.shell.extensions.dash-to-dock minimize-shift true
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
 gsettings set org.gnome.shell.extensions.dash-to-dock app-shift-hotkey-7 "['<Shift><Super>7']"
 gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-10 "['<Super>0']"
-gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-customize-running-dots true                                                        
+gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-customize-running-dots true
 gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-4 "['<Super>4']"
 gsettings set org.gnome.shell.extensions.dash-to-dock app-ctrl-hotkey-7 "['<Ctrl><Super>7']"
-gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity 0.69999999999999996                                                          
-gsettings set org.gnome.shell.extensions.dash-to-dock middle-click-action 'launch'                                                                    
-gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false                                                                             
-gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed true
+gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity 1.0
+gsettings set org.gnome.shell.extensions.dash-to-dock middle-click-action 'launch'
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
 gsettings set org.gnome.shell.extensions.dash-to-dock app-shift-hotkey-8 "['<Shift><Super>8']"
-gsettings set org.gnome.shell.extensions.dash-to-dock max-alpha 0.80000000000000004
+gsettings set org.gnome.shell.extensions.dash-to-dock max-alpha 1.0
 gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-5 "['<Super>5']"
 gsettings set org.gnome.shell.extensions.dash-to-dock app-ctrl-hotkey-8 "['<Ctrl><Super>8']"
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 32
@@ -76,9 +77,9 @@ gsettings set org.gnome.shell.extensions.dash-to-dock require-pressure-to-show t
 gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-6 "['<Super>6']"
 gsettings set org.gnome.shell.extensions.dash-to-dock app-ctrl-hotkey-9 "['<Ctrl><Super>9']"
 gsettings set org.gnome.shell.extensions.dash-to-dock bolt-support true
-gsettings set org.gnome.shell.extensions.dash-to-dock scroll-switch-workspace true 
+gsettings set org.gnome.shell.extensions.dash-to-dock scroll-switch-workspace true
 gsettings set org.gnome.shell.extensions.dash-to-dock preferred-monitor 0
-gsettings set org.gnome.shell.extensions.dash-to-dock min-alpha 0.20000000000000001
+gsettings set org.gnome.shell.extensions.dash-to-dock min-alpha 0.0
 gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-7 "['<Super>7']"
 gsettings set org.gnome.shell.extensions.dash-to-dock app-ctrl-hotkey-10 "['<Ctrl><Super>0']"
 gsettings set org.gnome.shell.extensions.dash-to-dock activate-single-window true
@@ -88,5 +89,11 @@ gsettings set org.gnome.shell.extensions.dash-to-dock background-color '#ffffff'
 gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-8 "['<Super>8']"
 gsettings set org.gnome.shell.extensions.dash-to-dock app-shift-hotkey-10 "['<Shift><Super>0']"
 gsettings set org.gnome.shell.extensions.dash-to-dock hot-keys true
+
 # Auto move windows.
 gsettings set org.gnome.shell.extensions.auto-move-windows application-list "['webstorm_webstorm.desktop:2', 'goland_goland.desktop:2', 'sublime_text.desktop:2', 'google-chrome.desktop:1', 'syntevo-smartgit.desktop:1', 'org.gnome.Terminal.desktop:1']"
+
+# Desktop icons.
+gsettings set org.gnome.shell.extensions.desktop-icons show-trash false
+gsettings set org.gnome.shell.extensions.desktop-icons show-home false
+gsettings set org.gnome.shell.extensions.desktop-icons icon-size 'small'
