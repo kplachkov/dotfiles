825c25a8d1fae2f1cc513ff6c943dc7e8fa2074b
kplachkov <k.n.plachkov@gmail.com>
2020-08-23 19:53:39 +0200

    Change settings, add helper functions and refactor

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+dotfiles_dir=$(dirname "$(readlink -f "$0")")
+
+. "$dotfiles_dir/setup/utils.sh"
+
 function main {
 	disable_lock
 
@@ -11,11 +15,7 @@ function main {
 }
 
 function setup_configuration {
-	echo "Setting up:"
-	format=" - %s\n"
-
-	printf "$format" "dotfiles"
-	dotfiles_dir=$(dirname "$(readlink -f "$0")")
+	echo "Setting up dotfiles"
 	dotfiles=(.aliases .bashrc .bash_options .exports .imwheelrc .inputrc .tmux.conf)
 
 	for dotf in "${dotfiles[@]}"
@@ -23,24 +23,24 @@ function setup_configuration {
 	    /bin/ln -fs "$dotfiles_dir/${dotf}" ~/
 	done
 
-	printf "$format" "htop configuration"
+	echo "Setting up htop configuration"
 	htop_config_dir=~/.config/htop/
 	mkdir -p $htop_config_dir
 	/bin/ln -fs "$dotfiles_dir/htoprc" $htop_config_dir
 
-	printf "$format" "sublime configuration"
+	echo "Setting up sublime configuration"
 	subl_config_dir=~/.config/sublime-text-3/Packages/User/
 	mkdir -p $subl_config_dir
 	/bin/ln -fs "$dotfiles_dir/config/sublime-text-3/Default (Linux).sublime-keymap" $subl_config_dir
 	/bin/ln -fs "$dotfiles_dir/config/sublime-text-3/Preferences.sublime-settings" $subl_config_dir
 	/bin/ln -fs "$dotfiles_dir/config/sublime-text-3/Package Control.sublime-settings" $subl_config_dir
 
-	printf "$format" "autostart applications and scripts"
+	echo "Setting up autostart applications and scripts"
 	autostart_dir=~/.config/autostart/
 	mkdir -p $autostart_dir
 	/bin/ln -fs "$dotfiles_dir/imwheel.desktop" $autostart_dir
 
-	printf "$format" "cursor configuration"
+	echo "Setting up cursor configuration"
 	mkdir -p /etc/default/
 	sudo /bin/ln -fs "$dotfiles_dir/unclutter" /etc/default/
 
@@ -57,18 +57,6 @@ function install {
 
 	echo "Setting up default applications"
 	sudo bash "$dotfiles_dir/setup/default_apps.sh"
-}
-
-function disable_lock {
-	gsettings set org.gnome.desktop.screensaver lock-enabled false
-	gsettings set org.gnome.desktop.session idle-delay 0
-	echo "Lock screen is disabled"
-}
-
-function enable_lock {
-	gsettings set org.gnome.desktop.screensaver lock-enabled true
-	gsettings set org.gnome.desktop.session idle-delay 120
-	echo "Lock screen is enabled"
 }
 
 main
--- setup/settings.sh
+++ setup/settings.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+setup_dir=$(dirname "$(readlink -f "$0")")
+scripts_dir=${setup_dir/setup/scripts}
+
 # Keybindings.
 gsettings set org.gnome.settings-daemon.plugins.media-keys terminal "[]"
 gsettings set org.gnome.desktop.wm.keybindings switch-applications "[]"
@@ -21,13 +24,10 @@ gsettings set org.gnome.desktop.wm.keybindings show-desktop "['<Super>d']"
 gsettings set org.gnome.desktop.wm.keybindings cycle-windows "['<Super>Tab']"
 gsettings set org.gnome.desktop.wm.keybindings cycle-windows-backward "['<Shift><Super>Tab']"
 gsettings set org.gnome.desktop.wm.keybindings toggle-fullscreen "['<Alt>F11']"
-gsettings set org.gnome.desktop.wm.keybindings close "['<Primary>space']"
+gsettings set org.gnome.desktop.wm.keybindings close "['<Primary><Super>space']"
 
 # Custom keybindings.
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/']"
-# Scripts directory path.
-setup_dir=$(dirname "$(readlink -f "$0")")
-scripts_dir=${setup_dir/setup/scripts}
 # Shutdown computer.
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "shutdown"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "shutdown now"
@@ -66,7 +66,10 @@ gsettings set org.gnome.desktop.interface gtk-theme "Yaru-dark"
 gsettings set org.gnome.desktop.interface cursor-size 64
 gsettings set org.gnome.desktop.interface clock-show-date true
 gsettings set org.gnome.desktop.interface clock-show-weekday true
+gsettings set org.gnome.desktop.interface clock-format '24h'
 gsettings set org.gnome.desktop.interface show-battery-percentage true
+gsettings set org.gnome.desktop.interface document-font-name 'Sans 12'
+gsettings set org.gnome.desktop.interface font-name 'Ubuntu 12'
 
 # Privacy.
 gsettings set org.gnome.desktop.privacy send-software-usage-stats false
