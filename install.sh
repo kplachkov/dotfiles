510ae885a015d026ab97a257b54784f643b4d1e2
kplachkov <k.n.plachkov@gmail.com>
2020-09-06 13:59:00 +0200

    Fix installation

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo apt update
 sudo apt install -y git
 
 echo "Downloading files"
-git clone dotfiles_repository dotfiles_dir
+git clone $dotfiles_repository $dotfiles_dir
 
 echo "Installing setup"
-bash dotfiles_dir/setup/install.sh
+bash "$dotfiles_dir/setup/install.sh"
